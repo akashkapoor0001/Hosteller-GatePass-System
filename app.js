1d79e7ba28b90cba31b2d63f5f985959e54ab2fe
AKASH KAPOOR <akashkapoor976@gmail.com>
2024-04-27 02:08:50 +0530

    Updated the files

---
+++ app.js
@@ -0,0 +1,464 @@
+const mysql = require('mysql');
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+flash = require('express-flash')
+
+
+const connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'Root@123',
+	database : 'userdb'
+});
+
+const app = express();
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Serve static files with correct MIME type
+app.use(express.static(path.join(__dirname, 'static'), {
+    setHeaders: (res, path, stat) => {
+        if (path.endsWith('.css')) {
+            res.setHeader('Content-Type', 'text/css');
+        }
+    }
+}));
+
+app.use(
+    session({
+      resave: true,
+      saveUninitialized: true,
+      secret:"yash is a super star",
+      cookie: { secure: false, maxAge: 14400000 },
+    })
+);
+app.use(flash());
+
+
+// http://localhost:3000/
+app.get('/', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + 'http://localhost:8080/index.html'));
+});
+
+// http://localhost:3000/slogin
+app.post('/slogin', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+// http://localhost:3000/auth
+app.post('/sauth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM student WHERE user = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/shome');
+				//response.render("/shome");
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+
+// http://localhost:3000/signup
+app.post('/ssignup', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/signup.html'));
+});
+
+// http://localhost:3000/register
+app.post('/register', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+    let confirm_password = request.body.confirm_password;
+	
+	// Ensure the input fields exists and are not empty
+	if (username && password && confirm_password) {
+		console.log(username,password);
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM student WHERE user = ?', [username], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				response.redirect('/alreadyexists');
+			} else if(confirm_password!=password) {
+				response.redirect('/passnotmatched');
+			}	else {
+                var users={
+                    user : request.body.username,
+                    password: request.body.password
+                  }
+				  
+                var sql = 'INSERT INTO student SET ?';
+                connection.query(sql,users, function(error,results){
+                    if (error) throw error;
+                    
+                });
+				response.redirect('/afterreg');
+            }		
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+// http://localhost:3000/tlogin
+app.post('/tlogin', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/tlogin.html'));
+});
+
+app.get('/afterreg', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/afterreg.html'));
+});
+app.get('/passnotmatched', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/passnotmatched.html'));
+});
+app.get('/alreadyexists', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/alreadyexists.html'));
+});
+
+// http://localhost:3000/tauth
+app.post('/tauth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		
+		if(username=='teacher' && password=='1234'){
+			request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/thome');
+
+		} else {
+			response.send('Incorrect Username and/or Password!');
+		}			
+		response.end();
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+// http://localhost:3000/hlogin
+app.post('/hlogin', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/hlogin.html'));
+});
+
+// http://localhost:3000/hauth
+app.post('/hauth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+	
+		if(username=='hod' && password=='hod'){
+			request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/hhome');
+
+		} else {
+			response.send('Incorrect Username and/or Password!');
+		}			
+		response.end();
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+// http://localhost:3000/glogin
+app.post('/glogin', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/glogin.html'));
+});
+
+// http://localhost:3000/gauth
+app.post('/gauth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		
+		if(username=='gate' && password=='gate'){
+			request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/ghome');
+
+		} else {
+			response.send('Incorrect Username and/or Password!');
+		}			
+		response.end();
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+
+// http://localhost:3000/home
+app.get('/home', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+// http://localhost:3000/shome
+/*app.get('/shome', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		response.send('Welcome back to student main page, ' + request.session.username + '!');
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});*/
+
+// http://localhost:3000/
+app.get('/shome', function(request, response) {
+	var un = request.session.username;
+	console.log(un);
+	// Render login template
+	//response.sendFile(path.join(__dirname + '/mainStd.html'));
+	response.render(__dirname+"/mainStd.ejs",{name:un});
+});
+app.post('/aply', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/apply.html'));
+});
+
+app.post('/filled', function(request, response) {
+	// Render login template
+	var roll = request.body.roll;
+	var name = request.body.name;
+	var date = request.body.date;
+	var section = request.body.section;
+	var phnum = request.body.phnum;
+	var reason = request.body.reason;
+	console.log(date);
+	//response.sendFile(path.join(__dirname + '/filled.ejs'));
+	response.render(__dirname+"/filled.ejs",{roll:roll,name:name,date:date,section:section,phnum:phnum,reason:reason});
+	var details={
+		roll: roll,
+		name : name,
+		class: section,
+		phnum: phnum,
+		reason: reason,
+		date: date,
+		
+	  }
+	var sql = 'INSERT INTO gatepass SET ?';
+	connection.query(sql,details, function(error,results){
+		if (error) throw error;
+		console.log("entered in db");
+	});
+});
+
+app.post('/update', function(request, response) {
+	// Render login template
+	var auto = request.body.accepting;
+    //var location = document.location;
+	var sql = 'UPDATE gatepass SET status = ? where auto = ?'
+	connection.query(sql,['hod',auto], function(error,results){
+		if (error) throw error;
+		else{
+			response.redirect("/thome");
+			//res.json(result);
+		  }
+	});
+	//response.sendFile(path.join(__dirname + '/updated.html'));
+	//response.render(__dirname+"/updated.html");
+	
+});
+app.post('/tdecline', function(request, response) {
+	// Render login template
+	var auto = request.body.declining;
+    //var location = document.location;
+	var sql = 'UPDATE gatepass SET status = ? where auto = ?'
+	connection.query(sql,['declined from teacher',auto], function(error,results){
+		if (error) throw error;
+		else{
+			response.redirect("/thome");
+			//res.json(result);
+		  }
+	});
+	//response.sendFile(path.join(__dirname + '/updated.html'));
+	//response.render(__dirname+"/updated.html");
+	
+});
+
+app.post('/hdecline', function(request, response) {
+	// Render login template
+	var auto = request.body.declining;
+    //var location = document.location;
+	var sql = 'UPDATE gatepass SET status = ? where auto = ?'
+	connection.query(sql,['declined from hod',auto], function(error,results){
+		if (error) throw error;
+		else{
+			response.redirect("/hhome");
+			
+		  }
+	});
+	
+	
+});
+
+
+
+app.post('/status', function(request, response) {
+    var un = request.session.username; // Retrieve the username from the session
+    console.log('status', un); // Log the username for debugging purposes
+
+    // Query the database to get the gate pass status for the current user
+	connection.query('SELECT * FROM gatepass where roll=?', [un], function(err, rows) {
+		if (err) {
+			// If there is an error, flash an error message and render the status page with no data
+			request.flash('error', err); 
+			response.render(__dirname + "/status.ejs", { page_title: "Users - Node.js", data: '' });   
+		} else {
+			// If the query is successful, render the status page with the retrieved data
+			console.log('Gatepass Data: ', rows); // Log the retrieved data for debugging purposes
+			response.render(__dirname + "/status.ejs", { page_title: "Users - Node.js", data: rows });
+		}
+	});
+});	
+
+
+app.get('/thome', function(request, response) {
+	// Render login template
+	//response.sendFile(path.join(__dirname + '/mainTea.html'));
+	connection.query('SELECT * FROM gatepass where status is null',function(err,rows)     {
+ 
+        if(err){
+         request.flash('error', err); 
+         response.render(__dirname+"/mainTea.ejs",{page_title:"Users - Node.js",data:''});   
+        }else{
+            //console.log(rows);
+            response.render(__dirname+"/mainTea.ejs",{page_title:"Users - Node.js",data:rows});
+        }
+                            
+         });
+        
+    });
+ 
+
+app.get('/hhome', function(request, response) {
+	// Render login template
+	//response.sendFile(path.join(__dirname + '/mainhod.html'));
+	var val = 'hod';
+	connection.query('SELECT * FROM gatepass where status = ?',[val],function(err,rows)     {
+ 
+        if(err){
+         request.flash('error', err); 
+         response.render(__dirname+"/mainhod.ejs",{page_title:"Users - Node.js",data:''});   
+        }else{
+            //console.log(rows);
+            response.render(__dirname+"/mainhod.ejs",{page_title:"Users - Node.js",data:rows});
+        }
+                            
+         });
+});
+
+app.post('/updatehod', function(request, response) {
+	// Render login template
+	var auto = request.body.accepting;
+    //var location = document.location;
+	var sql = 'UPDATE gatepass SET status = ? where auto = ?'
+	connection.query(sql,['gate',auto], function(error,results){
+		if (error) throw error;
+		else{
+			response.redirect("/hhome");
+			//res.json(result);
+		  }
+	});
+	//response.sendFile(path.join(__dirname + '/updated.html'));
+	//response.render(__dirname+"/updated.html");
+	
+});
+
+
+app.get('/ghome', function(request, response) {
+	// Render login template
+	//response.sendFile(path.join(__dirname + '/maingate.html'));
+	var val = 'gate';
+	connection.query('SELECT * FROM gatepass where status = ?',[val],function(err,rows)     {
+ 
+        if(err){
+         request.flash('error', err); 
+         response.render(__dirname+"/maingate.ejs",{page_title:"Users - Node.js",data:''});   
+        }else{
+            //console.log(rows);
+            response.render(__dirname+"/maingate.ejs",{page_title:"Users - Node.js",data:rows});
+        }
+                            
+         });
+});
+
+app.post('/updategate', function(request, response) {
+	// Render login template
+	var auto = request.body.out;
+    //var location = document.location;
+	var sql = 'UPDATE gatepass SET status = ?, outtime = CURRENT_TIMESTAMP where auto = ?'
+	connection.query(sql,['issuefinish',auto], function(error,results){
+		if (error) throw error;
+		else{
+			response.redirect("/ghome");
+			//res.json(result);
+		  }
+	});
+	//response.sendFile(path.join(__dirname + '/updated.html'));
+	//response.render(__dirname+"/updated.html");
+	
+});
+//port number
+app.listen(8080, () => {
+	console.log('Server is running on http://localhost:8080');
+  });
+  
+
---
+++ userTable.sql
@@ -0,0 +1,106 @@
+-- MySQL dump 10.13  Distrib 8.0.35, for Win64 (x86_64)
+--
+-- Host: localhost    Database: userdb
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `gatepass`
+--
+
+DROP TABLE IF EXISTS `gatepass`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gatepass` (
+  `auto` int NOT NULL AUTO_INCREMENT,
+  `roll` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `phnum` varchar(255) NOT NULL,
+  `reason` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `outtime` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`auto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gatepass`
+--
+
+LOCK TABLES `gatepass` WRITE;
+/*!40000 ALTER TABLE `gatepass` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gatepass` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usertable`
+--
+
+DROP TABLE IF EXISTS `usertable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usertable` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usertable`
+--
+
+LOCK TABLES `usertable` WRITE;
+/*!40000 ALTER TABLE `usertable` DISABLE KEYS */;
+INSERT INTO `usertable` VALUES (1,'john123','password123'),(2,'alice456','secret456'),(3,'bob789','mypassword'),(4,'sarah','sarah123'),(5,'mike','qwerty');
+/*!40000 ALTER TABLE `usertable` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-26 23:26:10
